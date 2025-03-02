2fbf893d278a5e14254b5b2f1e06119e1a4bb5e7
HoSung23 <150013982+HoSung23@users.noreply.github.com>
2025-03-02 11:26:55 -0600

    Add files via upload

---
+++ Registros.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorApp1.Data.Models
+{
+    public class Registros
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public required string Asunto { get; set; }
+        public required string Cliente { get; set; }
+        public required string DescCorta { get; set; }
+        public required string Descripcion { get; set; }
+        public string Estado { get; set; } = string.Empty;
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string Nota { get; set; } = string.Empty;
+    }
+}
